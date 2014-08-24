c4b07dadfc843331ccf050fe9434a12ca34bb51b
Julian Møller <julian@julianmoeller.dk>
2014-08-24 22:40:15 +0200

    Can read config file and accept POST webhook requests from Bitbucket

--- golive.go
+++ golive.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-  "fmt"
   "net/http"
   "log"
   "io/ioutil"
   "encoding/json"
+  "regexp"
+  "fmt"
+  "os/exec"
 )
 
 // A map of repos => (a map of branches => (a list of actions))
@@ -14,14 +16,14 @@ type Config map[string]map[string][]string
 
 
 type HookMsg struct {
-  CanonicalUrl string "canon_url"
+  CanonicalUrl string `json:"canon_url"`
 
   Commits []struct {
     Branch string
   }
 
   Repository struct {
-    AbsoluteUrl string "absolute_url"
+    AbsoluteUrl string `json:"absolute_url"`
   }
 }
 
@@ -29,66 +31,68 @@ type Commit struct {
   Repository string
   Branch string
 }
-/*
-{
-    "canon_url": "https://bitbucket.org",
-    "commits": [
-        {
-            "author": "marcus",
-            "branch": "master",
-            "files": [
-                {
-                    "file": "somefile.py",
-                    "type": "modified"
-                }
-            ],
-            "message": "Added some more things to somefile.py\n",
-            "node": "620ade18607a",
-            "parents": [
-                "702c70160afc"
-            ],
-            "raw_author": "Marcus Bertrand <marcus@somedomain.com>",
-            "raw_node": "620ade18607ac42d872b568bb92acaa9a28620e9",
-            "revision": null,
-            "size": -1,
-            "timestamp": "2012-05-30 05:58:56",
-            "utctimestamp": "2012-05-30 03:58:56+00:00"
-        }
-    ],
-    "repository": {
-        "absolute_url": "/marcus/project-x/",
-        "fork": false,
-        "is_private": true,
-        "name": "Project X",
-        "owner": "marcus",
-        "scm": "git",
-        "slug": "project-x",
-        "website": "https://atlassian.com/"
-    },
-    "user": "marcus"
-}
-*/
 
 func main() {
-
-  /*
-  - Modtag POST fra Bitbucket, unmarshal JSON til HookMsg
-
-  - Ryd op i HookMsg og konstruer (evt. flere) Commit objekter
-
-  - For hver Commit, hvis config[Commit.Repository][Commit.Branch] findes,
-    lav map[string]int af actions -> antal gange den er blevet bedt om at blive
-    kørt
-
-    For hver action, kør den
-  */
   config_raw, _ := ioutil.ReadFile("golive.json")
   var config Config;
   json.Unmarshal(config_raw, &config)
 
-  http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	   fmt.Fprintf(w, "Hello, %q, %v, %q", r.URL.Path, config, config_raw)
+  msgs := make(chan HookMsg, 100)
+  commits := make(chan Commit, 100)
+  jobs := make(chan string, 100)
+
+  go hookWrangler(msgs, commits)
+  go commitWrangler(commits, jobs, config)
+  go jobRunner(jobs)
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    payload := r.FormValue("payload")
+
+    var hookmsg HookMsg
+    if err := json.Unmarshal([]byte(payload), &hookmsg); err != nil {
+      http.Error(w, "Could not decode json", 500)
+			log.Print(err)
+			return
+    }
+
+    msgs <- hookmsg
   })
 
   log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hookWrangler(msgs <-chan HookMsg, results chan<- Commit) {
+  reg := regexp.MustCompile("https?://")
+
+  for msg := range msgs {
+    baserepo := reg.ReplaceAllString(msg.CanonicalUrl, "")
+    repository := baserepo + msg.Repository.AbsoluteUrl
+
+    for _, commit := range msg.Commits {
+      results <- Commit{ repository, commit.Branch}
+    }
+  }
+}
+
+func commitWrangler(commits <-chan Commit, results chan<- string, config Config) {
+  for commit := range commits {
+    if commit.Branch == "" || commit.Repository == "" {
+      continue
+    }
+
+    if branches, ok := config[commit.Repository]; ok {
+      if actions, ok := branches[commit.Branch]; ok {
+        for _, action := range actions {
+          results <- action
+        }
+      }
+    }
+  }
+}
+
+func jobRunner(jobs <-chan string) {
+  for job := range jobs {
+    command := exec.Command("bash", "-c", job)
+    go command.Run()
+  }
 }
